8840f438498f95dc12e6c6bfcdf3bae519d29f7d
Rana718 <ranadolui718@gmail.com>
2025-10-07 18:08:11 +0530

    new version

--- backend/main.py
+++ backend/main.py
@@ -36,7 +36,7 @@ async def startup_event():
 async def root():
     return {
         "message": "Welcome to the AI CV Builder API!",
-        "version": "1.0.4",
+        "version": "1.0.7",
     } 
 
 @app.get("/private")
